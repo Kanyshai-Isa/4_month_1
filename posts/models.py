54f2d9aabbf740065010e2cacafb84d15096fd61
Kanyshai-Isa <kanyshaiisaevak@gmail.com>
2025-11-07 13:10:28 +0600

    HW2 vernyi

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -20,6 +21,8 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     content = models.CharField(max_length=1000, null=True, blank=True)
     rate = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.title} - {self.content}"
